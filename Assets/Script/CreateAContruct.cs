4337268c8e2002ce88f635ae61743bd04c86f5ca
Léo <leolecointre50@gmail.com>
2023-03-10 09:08:08 +0100

    merge

--- Assets/Script/CreateAContruct.cs
+++ Assets/Script/CreateAContruct.cs
@@ -1,36 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class CreateAContruct : MonoBehaviour
 {
-    public GameObject cube;
+    GameObject cube;
     bool ConstructMod;
     public GameObject cubePrevisual;
     public Resource Resource;
+    public NavMeshSurface navMeshSurface;
+    public NavigationBaker navigationBaker;
     // Start is called before the first frame update
     void Start()
     {
-    
+        // navMeshSurface = cubePrevisual.GetComponent<NavMeshSurface>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 10000))
+        if (ConstructMod)
         {
-            if (ConstructMod)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10000))
             {
-                cubePrevisual.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y + 1, hit.transform.position.z);
-                if (Input.GetMouseButton(0))
+                string tagFromHit = hit.transform.gameObject.tag;
+                if (!(tagFromHit == "House" || tagFromHit == "Mine" || tagFromHit == "Forest" || tagFromHit == "Food" || tagFromHit == "Worksite"))
                 {
-                    cubePrevisual.SetActive(false);
-                    GameObject newCube = Instantiate(cube, new Vector3(hit.transform.position.x, hit.transform.position.y + 1, hit.transform.position.z), Quaternion.identity);
-                    ConstructMod = false;
+                    cubePrevisual.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y + 1, hit.transform.position.z);
 
+
+                    if (Input.GetMouseButton(0))
+                    {
+                        cubePrevisual.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y - 2, hit.transform.position.z);
+                        Resource.Wood = -cube.GetComponent<Construct>().woodCost;
+                        Resource.Stone = -cube.GetComponent<Construct>().stoneCost;
+                        Resource.Food = -cube.GetComponent<Construct>().foodCost;
+                        cubePrevisual.SetActive(false);
+                        GameObject newCube = Instantiate(cube, new Vector3(hit.transform.position.x, hit.transform.position.y + 1, hit.transform.position.z), Quaternion.identity);
+                        ConstructMod = false;
+                        navigationBaker.bakeTheNavigation();
+                    }
+                    if (Input.GetMouseButton(1))
+                    {
+                        cubePrevisual.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y - 2, hit.transform.position.z);
+                        cubePrevisual.SetActive(false);
+                        ConstructMod = false;
+                    }
                 }
             }
         }
@@ -42,9 +63,6 @@ public class CreateAContruct : MonoBehaviour
         if (construct.woodCost <= Resource.Wood && construct.stoneCost <= Resource.Stone && construct.foodCost <= Resource.Food && !ConstructMod) 
         {
             cube = aConstruct;
-            Resource.Wood = -construct.woodCost;
-            Resource.Stone = -construct.stoneCost;
-            Resource.Food = -construct.foodCost;
             ConstructMod = true;
             cubePrevisual.SetActive(true);
         }
