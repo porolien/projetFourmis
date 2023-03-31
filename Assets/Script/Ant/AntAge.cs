cdc72afeced3aeeb5dcbaa9820f4fbafd7d3de4c
Maxime HARRY <maxime.harry@e-artsup.net>
2023-03-31 11:25:42 +0200

    Debug pause and add age ant

---
+++ Assets/Script/Ant/AntAge.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntAge : MonoBehaviour
+{
+    [SerializeField] private int minOld = 15;
+    [SerializeField] private int maxOld = 20;
+    public int maxAge;
+    public int age = 0;
+
+    void Start()
+    {
+        maxAge = Random.Range(minOld, maxOld);
+    }
+
+    public void GainAge()
+    {
+        age += 1;
+        if (age >= maxAge)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
