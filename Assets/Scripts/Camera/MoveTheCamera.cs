6b65223326aa373b139da3d5c1127f77d2910521
Léo <leolecointre50@gmail.com>
2023-04-16 09:59:13 +0200

    debut de la jolie map

--- Assets/Scripts/Camera/MoveTheCamera.cs
+++ Assets/Scripts/Camera/MoveTheCamera.cs
@@ -25,11 +25,11 @@ public class MoveTheCamera : MonoBehaviour
 
         if (mousePosX < scrollDistance)
         {
-            transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);
+            transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime * 1.5f);
         }
         if (mousePosX >= Screen.width - scrollDistance)
         {
-            transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
+            transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime * 1.5f);
         }
         if (mousePosY < scrollDistance)
         {
@@ -49,13 +49,13 @@ public class MoveTheCamera : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -35);
         }
-        if (transform.position.x >= 0)
+        if (transform.position.x >= -14)
         {
-            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-14, transform.position.y, transform.position.z);
         }
-        if (transform.position.z >= 0)
+        if (transform.position.z >= -14)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -14);
         }
 
         // Zoom the camera
